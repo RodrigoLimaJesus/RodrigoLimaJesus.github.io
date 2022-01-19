6525d2126f122c3d80f55ef9abcbb4a16c164361
Rodrigo Lima <rodrigootavio19@gmail.com>
2022-01-19 01:41:17 -0400

    feat: adiciona menu lateral com links pessoais

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import { GiHamburgerMenu } from "react-icons/gi"
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+import SideBar from './SideBar';
 
 export default function Header() {
+  const [showSideBar, setShowSideBar] = useState(false);
+
   function handleMenu({ target }) {
     const btn = target.closest('button');
+
+    if (!btn.classList.contains('show-menu')) {
+      setShowSideBar(true);
+    } else {
+      setShowSideBar(false);
+    }
 
     btn.classList.toggle('show-menu');
   }
 
   return (
     <header className="header">
-        <h1>Rodrigo <span className="orange">Lima</span></h1>
-        <button
-          type="button"
-          className="btn-header-menu"
-          onClick={ handleMenu }
-        >
-          <GiHamburgerMenu />
-        </button>
-      </header>
+      <h1>Rodrigo <span className="orange">Lima</span></h1>
+
+      <button
+        type="button"
+        className="btn-header-menu"
+        onClick={ handleMenu }
+      >
+        <GiHamburgerMenu />
+      </button>
+
+      <SideBar isVisible={ showSideBar } />
+    </header>
   )
 }
