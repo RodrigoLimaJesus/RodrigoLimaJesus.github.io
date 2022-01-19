4d5e31342dc355a7a27f858496ce21da873609ae
Rodrigo Lima <rodrigootavio19@gmail.com>
2022-01-19 03:16:30 -0400

    feat: add seçao de apresentacao e stacks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import Header from "./components/Header";
+import {
+  DiJavascript,
+  DiHtml5,
+  DiCss3,
+  DiReact,
+  DiGit,
+} from 'react-icons/di';
+import { SiRedux, SiGithub } from 'react-icons/si';
+
+import Header from './components/Header';
+import ProgrammigGif from './images/programming.gif';
 
 export default function App() {
   return (
     <div>
       <Header />
+      <div className="container apresentation-container">
+        <img
+          src={ProgrammigGif}
+          alt="Gif de pessoa programando"
+        />
+        <p>Estudante de desenvolvimento web na <a className="green" href="https://www.betrybe.com/">Trybe</a>, atualmente formado como dev front-end Jr e me aprofundando nos conhecimentos básicos do back-end.</p>
+      </div>
+
+      <div className="container skills-container">
+        <h2 className="green">{"<Stacks>"}</h2>
+        <div>
+          <span className="blue"><DiCss3 />CSS</span>
+          <span className="orange"><DiHtml5 />HTML</span>
+          <span className="yellow"><DiJavascript />JAVASCRIPT</span>
+          <span className="blue"><DiReact />REACT</span>
+          <span className="orange"><DiGit />GIT</span>
+          <span className="purple"><SiRedux />REDUX</span>
+          <span><SiGithub />GITHUB</span>
+        </div>
+        <h2 className="green">{"</Stacks>"}</h2>
+      </div>
     </div>
   );
 }
