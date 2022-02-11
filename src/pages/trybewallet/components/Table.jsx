a88ae0c4ce546a80e25404af7b2f77914dba2eb6
Rodrigo Lima <rodrigootavio19@gmail.com>
2022-02-10 21:47:45 -0400

    feat: finaliza base trybewallet responsivo

--- src/pages/trybewallet/components/Table.jsx
+++ src/pages/trybewallet/components/Table.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 
-import { BsFillTrashFill, BsInfoCircle } from 'react-icons/bs';
+import { BsFillTrashFill, BsPencilFill } from 'react-icons/bs';
+import { TiArrowRightThick } from "react-icons/ti";
 
 import WalletContext from '../contexts/WalletContext';
 
@@ -11,26 +12,36 @@ export default function Table() {
     <table className="wallet-table">
       <thead>
         <tr>
-          <th>Despesas</th>
-          <th>Info / Deletar</th>
+          <th>Descrição</th>
+          <th className="wallet-table-1024">Tag</th>
+          <th className="wallet-table-1024">Método</th>
+          <th>Valor</th>
+          <th className="wallet-table-925">Origem <TiArrowRightThick /> Conversão</th>
+          <th>Editar / Deletar</th>
         </tr>
       </thead>
 
       <tbody>
         {expenses.map((expense, index) => {
           const { exchange, value } = expense;
+          console.log(expense);
           
           return (
             <tr key={ index }>
               <td>{expense.description}</td>
+              <td className="wallet-table-1024">{expense.tag}</td>
+              <td className="wallet-table-1024">{expense.method}</td>
               <td>{(Number(value) * Number(exchange.ask)).toFixed(2)}</td>
+              <td className="wallet-table-925">
+                {exchange.code} <TiArrowRightThick /> {exchange.codein}
+              </td>
               <td className="edit-delete-btns">
                 <button
                   type="button"
                   onClick={ () => {
                     setIsFormVisible({ isEditing: true, isVisible: true, expense });
                   }}>
-                  <BsInfoCircle />
+                  <BsPencilFill />
                 </button>
                 <button type="button" onClick={ () => rmvExpense(expense.id) }>
                   <BsFillTrashFill />
