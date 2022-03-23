3f9850e396a2de89f2668806c6766576bdf30ece
Rodrigo Lima <rodrigootavio19@gmail.com>
2022-03-22 23:51:25 -0400

    feat: add tailwind ao componente Apresentation

--- src/components/Apresentation.jsx
+++ src/components/Apresentation.jsx
@@ -3,20 +3,18 @@ import React from 'react';
 import MyImage from '../images/eu.jpeg';
 import ProgrammigGif from '../images/programming.gif';
 
+import Container from './Container';
+
 export default function Apresentation() {
   return (
-    <div className="container apresentation-container">
+    <Container>
       <img
         src={ProgrammigGif}
         alt="Gif de pessoa programando"
-        className="programmingGif"
+        className="w-10/12 m-auto rounded-xl"
       />
-      <img
-        src={MyImage}
-        alt="Minha imagem de perfil"
-        className="profile-image"
-      />
-      <p>
+      <img src={MyImage} alt="Minha imagem de perfil" className="hidden" />
+      <p className="font-bold my-5 text-justify">
         Sou brasileiro natural de Manaus/Am, atualmente estudo engenharia da
         computação na Universidade Federal do Amazonas e desenvolvimento web na{' '}
         <a href="https://www.betrybe.com/" target="_blank" rel="noreferrer">
@@ -27,6 +25,6 @@ export default function Apresentation() {
         desenvolvedor front-end Jr e agora me aprofundo nos conhecimentos do
         back-end para dar início à carreira full-stack.
       </p>
-    </div>
+    </Container>
   );
 }
