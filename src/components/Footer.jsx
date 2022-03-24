e4da38ec10d4458fde3a32b9f1bf152e394e8eda
Rodrigo Lima <rodrigootavio19@gmail.com>
2022-03-24 01:29:39 -0400

    style: add responsividade ao Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-app-container-dark rounded-t-2xl flex flex-col items-center text-center font-bold p-3">
+    <footer className="bg-app-container-dark rounded-t-2xl flex flex-col items-center text-center font-bold p-3 sm:text-lg md:flex-row md:justify-between md:px-5 lg:text-xl lg:px-8">
       <span className="my-1">
         Feito com React Js, Tailwind Css e dedicação.
       </span>
