b392d329a289ac82249b5783c6337a7a98e268ea
Rodrigo Lima <rodrigootavio19@gmail.com>
2022-03-23 23:33:08 -0400

    style: add responsividade ao Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -25,53 +25,57 @@ export default function Projects() {
     <Container>
       <ContainerTitle title="<Projetos>" />
 
-      <div>
-        {projects.map((project, index) => (
-          <div key={index} className="flex  flex-col-reverse">
-            <div className="mr-3 flex flex-col">
-              <h2 className="font-bold my-2 flex flex-row items-center justify-center">
-                {project.name}
-                {project.desktop && (
-                  <span className="mx-1">
-                    <FaDesktop />
-                  </span>
-                )}
-                {project.mobile && (
-                  <span className="mx-1">
-                    <FaMobileAlt />
-                  </span>
-                )}
-              </h2>
+      {projects.map((project, index) => (
+        <div
+          key={index}
+          className="flex  flex-col-reverse items-center md:my-4 md:flex-row"
+        >
+          <div className="mr-3 flex flex-col items-center justify-center md:w-2/4 md:text-lg lg:text-xl md:mr-5">
+            <p className="my-2 font-bold text-center md:my-0">
+              {project.description}
+            </p>
 
-              <p className="my-2 font-bold">{project.description}</p>
+            <ul className="list-[square] list-inside flex flex-row flex-wrap my-3">
+              {project.stacks.map((stack) => (
+                <li key={stack} className="mx-2 marker:text-app-links">
+                  {stack}
+                </li>
+              ))}
+            </ul>
 
-              <ul className="list-[square] list-inside flex flex-row flex-wrap my-3">
-                {project.stacks.map((stack) => (
-                  <li key={stack} className="mx-2">
-                    {stack}
-                  </li>
-                ))}
-              </ul>
+            <a
+              href={project.link}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="font-bold text-app-links self-center my-2 duration-300 sm:hover:text-app-links-hover sm:text-xl md:text-2xl"
+            >
+              Link do deploy
+            </a>
+          </div>
 
-              <a
-                href={project.link}
-                target="_blank"
-                rel="noreferrer noopener"
-                className="font-bold text-app-links self-center my-2"
-              >
-                Link do deploy
-              </a>
-            </div>
-
+          <div className="w-3/4 mt-3 md:w-2/4 md:mt-0 md:self-start">
             <img
               src={project.image}
               alt={project.name}
               title={project.name}
-              className="my-3 rounded-lg shadow-lg shadow-white/50"
+              className="rounded-lg shadow-lg shadow-white/50 w-full"
             />
+            <h2 className="font-bold my-2 flex flex-row items-center justify-center sm:text-xl md:text-2xl">
+              {project.name}
+              {project.desktop && (
+                <span className="mx-1">
+                  <FaDesktop />
+                </span>
+              )}
+              {project.mobile && (
+                <span className="mx-1">
+                  <FaMobileAlt />
+                </span>
+              )}
+            </h2>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
 
       <ContainerTitle title="</Projetos>" />
     </Container>
