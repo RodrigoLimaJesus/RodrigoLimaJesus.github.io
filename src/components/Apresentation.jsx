c616c7f1d1c16ef6106a8831e1dce50398d2fe64
Rodrigo Lima <rodrigootavio19@gmail.com>
2022-03-23 22:43:53 -0400

    style: add responsividade ao Apresentation

--- src/components/Apresentation.jsx
+++ src/components/Apresentation.jsx
@@ -8,23 +8,30 @@ import Container from './Container';
 export default function Apresentation() {
   return (
     <Container>
-      <img
-        src={ProgrammigGif}
-        alt="Gif de pessoa programando"
-        className="w-10/12 m-auto rounded-xl"
-      />
-      <img src={MyImage} alt="Minha imagem de perfil" className="hidden" />
-      <p className="font-bold my-5 text-justify">
-        Sou brasileiro natural de Manaus/Am, atualmente estudo engenharia da
-        computação na Universidade Federal do Amazonas e desenvolvimento web na{' '}
-        <a href="https://www.betrybe.com/" target="_blank" rel="noreferrer">
-          Trybe
-        </a>
-        , uma escola que vai além do ensino tradicional e incentiva muito o
-        desenvolvimento das soft skills. Sou formado pela mesma como
-        desenvolvedor front-end Jr e agora me aprofundo nos conhecimentos do
-        back-end para dar início à carreira full-stack.
-      </p>
+      <div className="md:flex md:flex-row">
+        <img
+          src={ProgrammigGif}
+          alt="Gif de pessoa programando"
+          className="w-10/12 m-auto rounded-xl sm:hidden"
+        />
+        <img
+          src={MyImage}
+          alt="Minha imagem de perfil"
+          className="hidden w-2/5 m-auto border-2 border-white rounded-lg sm:block md:mr-5 md:w-1/3 lg:w-1/4"
+        />
+        <p className="font-bold my-5 text-justify h-fit sm:text-lg xl:self-center xl:text-2xl">
+          Sou brasileiro natural de Manaus/Am, atualmente estudo engenharia da
+          computação na Universidade Federal do Amazonas e desenvolvimento web
+          na{' '}
+          <a href="https://www.betrybe.com/" target="_blank" rel="noreferrer">
+            Trybe
+          </a>
+          , uma escola que vai além do ensino tradicional e incentiva muito o
+          desenvolvimento das soft skills. Sou formado pela mesma como
+          desenvolvedor front-end Jr e agora me aprofundo nos conhecimentos do
+          back-end para dar início à carreira full-stack.
+        </p>
+      </div>
     </Container>
   );
 }
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Container({ children }) {
   return (
-    <div className="w-[90vw] mx-auto my-8 p-[4vw] flex flex-col bg-app-container-dark border-2 border-white rounded-2xl">
+    <div className="w-11/12 mx-auto my-8 p-[4vw] flex flex-col bg-app-container-dark border-2 border-white rounded-2xl sm:w-5/6 lg:w-9/12">
       {children}
     </div>
   );
