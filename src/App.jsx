5f82ee45e89f4ce12506d50158b7f420ae0534d2
Rodrigo Lima <rodrigootavio19@gmail.com>
2022-07-13 19:47:12 -0400

    feat: add navegaçao entre as seçoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import About from './components/about';
 import Contact from './components/contact';
+import GoPageTopBtn from './components/GoPageTopBtn';
 import Header from './components/header';
 import Projects from './components/projects';
 import Stacks from './components/stacks';
@@ -28,26 +29,28 @@ export default function App() {
   return (
     <div className={theme}>
       <div className="text-black dark:text-white">
+        <GoPageTopBtn />
         <section
+          id="page-top"
           className="
         bg-zinc-300 dark:bg-zinc-900
-        flex flex-col
-        transition
-        "
+          flex flex-col
+          transition
+          "
         >
           <Header changeTheme={changeTheme} />
           <Contact />
         </section>
 
-        <section className="bg-zinc-200 dark:bg-zinc-800 transition">
+        <section id="about" className="bg-zinc-200 dark:bg-zinc-800 transition">
           <About />
         </section>
 
-        <section className="bg-zinc-300 dark:bg-zinc-900 transition">
+        <section id="stacks" className="bg-zinc-300 dark:bg-zinc-900 transition">
           <Stacks />
         </section>
 
-        <section className="bg-zinc-200 dark:bg-zinc-800 transition">
+        <section id="projects" className="bg-zinc-200 dark:bg-zinc-800 transition">
           <Projects />
         </section>
       </div>
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMenu } from 'react-icons/io5';
+import NavBar from './components/NavBar';
 import ToggleThemeBtn from './components/ToggleThemeBtn';
 
 export default function Header({ changeTheme }) {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
   return (
-    <header className="flex justify-between items-center p-3">
+    <header
+      className="
+      flex justify-between items-center
+      p-3 md:px-6 lg:px-9
+      "
+    >
       <div className="flex items-center text-2xl sm:text-3xl">
-        <button className="mr-2">
+        <button className="mr-2 sm:hidden" onClick={() => setIsMenuVisible(true)}>
           <IoMenu />
         </button>
 
@@ -21,7 +29,11 @@ export default function Header({ changeTheme }) {
         </h1>
       </div>
 
-      <ToggleThemeBtn changeTheme={changeTheme} />
+      <div className="sm:flex items-center">
+        <NavBar isMenuVisible={isMenuVisible} setIsMenuVisible={setIsMenuVisible} />
+
+        <ToggleThemeBtn changeTheme={changeTheme} />
+      </div>
     </header>
   );
 }
