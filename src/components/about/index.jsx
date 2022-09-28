8feb107b21d3549c8ac9a18a4a598e24379b2dc5
Rodrigo Lima <rodrigootavio19@gmail.com>
2022-09-27 20:36:35 -0400

    docs: ajusta sobre com add de estagio

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -8,13 +8,7 @@ export default function About() {
       <Title>Sobre mim</Title>
 
       <p className="my-4 md:text-lg">
-        Sou brasileiro natural de Manaus/Am, apaixonado por programação e pelos desafios
-        que ela oferece, atualmente estudo engenharia da computação na Universidade
-        Federal do Amazonas e desenvolvimento web na Trybe, uma escola que vai além do
-        ensino tradicional e incentiva muito o desenvolvimento das soft skills. Na trybe
-        já me encontro no módulo de ciência da computação, tendo visto até então os
-        fundamentos do desenvolvimento web e a base para o desenvolvimento front-end e
-        back-end.
+      Sou brasileiro natural de Manaus/Am, apaixonado por programação e pelos desafios que ela oferece, atualmente estudo engenharia da computação na Universidade Federal do Amazonas e sou estagiário de NPI no Instituto de Pesquisas Eldorado. Como experiência certificada tenho o curso de desenvolvimento web na Trybe, uma escola que vai além do ensino tradicional e incentiva muito o desenvolvimento das soft skills, com assuntos em fundamentos do desenvolvimento web, desenvolvimentos front-end e back-end, e ciência da computação.
       </p>
     </Container>
   );
